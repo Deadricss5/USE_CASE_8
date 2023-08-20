a59c8910b94319bb86cef89cc8aaee7891a9dcbf
Serhii Suprun <ssupr@softserveinc.com>
2023-08-20 21:35:15 +0300

    UC#2 Infrastructure setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>USE CASE 8</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
